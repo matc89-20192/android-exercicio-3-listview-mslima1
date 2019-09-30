fee7cdee85e3df0e55bdfbc7004df084b9280690
Matheus <matheus_lima789@hotmail.com>
2019-09-29 23:52:58 -0300

    final

--- app/src/main/java/matc89/exercicio3/MainActivity.java
+++ app/src/main/java/matc89/exercicio3/MainActivity.java
@@ -2,12 +2,81 @@ package matc89.exercicio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Tarefa> tarefas;
+    private TarefaAdapter adapter;
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        tarefas = new ArrayList<Tarefa>();
+
+        adapter = new TarefaAdapter(this,  R.layout.adapter_view_layout, tarefas);
+        listView.setAdapter(adapter);
+        setupListViewListener();
     }
+
+    public void adicionar(View v) {
+        int erro = 0;
+
+        EditText editDescricao = (EditText) findViewById(R.id.editDescricao);
+        String itemText = editDescricao.getText().toString();
+        EditText prioridade = (EditText) findViewById(R.id.editPrioridade);
+        String prioridadeText = prioridade.getText().toString();
+        int pr = Integer.valueOf(prioridadeText).intValue();
+        if(pr<1 || pr>10){
+            Toast.makeText(this, "A prioridade deve estar entre 1 e 10.", Toast.LENGTH_SHORT).show();
+            erro=1;
+        }
+
+        for (Tarefa tarefa: tarefas) {
+            // Now you have the product. Just get the Id
+            if (tarefa.getDescricao().equals(itemText)){
+                Toast.makeText(this, "Tarefa já cadastrada.", Toast.LENGTH_SHORT).show();
+                erro = 1;
+            }
+        }
+        if(erro == 0) {
+            Tarefa tarefa = new Tarefa(itemText, pr);
+            adapter.add(tarefa);
+        }
+
+        editDescricao.setText("");
+        prioridade.setText("");
+    }
+
+    private void setupListViewListener() {
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter1,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        tarefas.remove(pos);
+                        adapter.notifyDataSetChanged();
+
+                    }
+
+                });
+    }
+
+    public void remover(View v) {
+        tarefas.remove(0);
+        adapter.notifyDataSetChanged();
+    }
+
 }
---
+++ app/src/main/java/matc89/exercicio3/TarefaAdapter.java
@@ -0,0 +1,77 @@
+package matc89.exercicio3;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.content.Context;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import matc89.exercicio3.Tarefa;
+
+public class TarefaAdapter extends ArrayAdapter<Tarefa>{
+    private Context mContext;
+    private int mResource;
+    private int lastPosition = -1;
+    Activity context;
+    ArrayList<Tarefa> tarefas;
+    private static LayoutInflater inflater = null;
+
+    private static class ViewHolder {
+        TextView descricao;
+        TextView prioridade;
+    }
+
+    public TarefaAdapter(Context context, int resource, ArrayList<Tarefa> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //get the persons information
+        String descricao = getItem(position).getDescricao();
+        int prioridade = getItem(position).getPrioridade();
+
+        //Create the person object with the information
+        Tarefa tarefa = new Tarefa(descricao,prioridade);
+
+        //create the view result for showing the animation
+        final View result;
+
+        //ViewHolder object
+        ViewHolder holder;
+
+
+        if(convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(mResource, parent, false);
+            holder= new ViewHolder();
+            holder.descricao = (TextView) convertView.findViewById(R.id.descricao);
+            holder.prioridade = (TextView) convertView.findViewById(R.id.prioridade);
+
+            result = convertView;
+
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+
+
+        holder.descricao.setText(tarefa.getDescricao());
+        int asa = tarefa.getPrioridade();
+        String p = Integer.toString(asa);
+        holder.prioridade.setText("Prioridade: " + p);
+
+
+
+        return convertView;
+    }
+}
